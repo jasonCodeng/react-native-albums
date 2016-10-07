2d3bfb6f523511fcd9ccac18ece8dbb9a386bc4d
Jason Deng <jdeng1234@gmail.com>
2016-10-07 01:51:01 -0400

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text} from 'react-native';
+
+const App = () => {
+    return (
+        <Text>Some Text</Text>
+    );
+};
+
+AppRegistry.registerComponent('albums', () => App);
